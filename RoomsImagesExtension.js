bf554a943b97c5ed3ee2e24fc4e1f0fadef0e6b5
JoaoMartins-Forge <joao.martins@autodesk.com>
2023-06-29 17:19:12 -0300

    update camera orientation

--- RoomsImagesExtension.js
+++ RoomsImagesExtension.js
@@ -3,9 +3,14 @@ class RoomsImagesExtension extends Autodesk.Viewing.Extension {
     super(viewer, options);
     this._onObjectTreeCreated = (ev) => this.onModelLoaded(ev.model);
     this.viewObjs = null;
+    this.roomElements = [];
   }
 
   async onModelLoaded(model) {
+    // this.viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, async () => {
+    //   this.doorData = await this.getDoorData();
+    // });
+    this.doorData = await this.getDoorData();
     this.viewer.addEventListener(Autodesk.Viewing.CAMERA_TRANSITION_COMPLETED, () => {
       this.generateRoomsThumbnails.call(this)
     });
@@ -22,14 +27,34 @@ class RoomsImagesExtension extends Autodesk.Viewing.Extension {
   async getRoomsCameraArrays() {
     this.viewObjs = [];
     const roomsData = await this.getRoomData();
+    //Here we set the height in meters, so if the model is set in ft, we need a multiplier
+    let unitsMultiplier = 1;
+    let eyeHeight = 1.7;
+    switch (this.viewer.model.getUnitString()) {
+      case 'ft':
+        unitsMultiplier = 3.28084;
+      default:
+        break;
+    }
     for (const room of roomsData) {
       try {
         let roombbox = await this.getBoundingBox(room.dbId);
+        let roomDoors = this.doorData.filter(door => door.bbox.intersectsBox(roombbox));
         let newViewObj = {};
-        newViewObj.target = roombbox.getCenter();
-        newViewObj.position = roombbox.max.clone().add(newViewObj.target.clone()).multiplyScalar(0.5);
         newViewObj.name = room.name;
         newViewObj.dbId = room.dbId;
+        newViewObj.target = roombbox.getCenter();
+        let auxposition;
+        if (roomDoors.length > 0) {
+          const doorCenter = roomDoors[0].getCenter();
+          let auxDirection = newViewObj.target.clone().sub(doorCenter).normalize();
+          auxposition = doorCenter.add(auxDirection.multiplyScalar(1 * unitsMultiplier));
+        }
+        else {
+          auxposition = roombbox.max.clone().add(newViewObj.target.clone()).multiplyScalar(0.5);
+        }
+        auxposition.z = roombbox.min.z + unitsMultiplier * eyeHeight;
+        newViewObj.position = auxposition;
         this.viewObjs.push(newViewObj);
       }
       catch (error) {
@@ -41,6 +66,7 @@ class RoomsImagesExtension extends Autodesk.Viewing.Extension {
   onToolbarCreated(toolbar) {
     this._button = this.createToolbarButton('roomsthumbnails-button', 'https://img.icons8.com/ios/30/camera--v3.png', 'Rooms Screenshots');
     this._button.onClick = async () => {
+      this.roomElements = [];
       await this.getRoomsCameraArrays();
       this.viewer.navigation.toPerspective();
       await this.generateRoomsThumbnails();
@@ -121,6 +147,58 @@ class RoomsImagesExtension extends Autodesk.Viewing.Extension {
     return data;
   }
 
+  async getDoorData() {
+    const getDoorDbIds = () => {
+      return new Promise((resolve, reject) => {
+        this.viewer.search(
+          'Doors',
+          (dbIds) => resolve(dbIds),
+          (error) => reject(error),
+          ['Category'],
+          { searchHidden: true }
+        );
+      });
+    };
+
+    const getPropertiesAsync = (dbId) => {
+      return new Promise((resolve, reject) => {
+        this.viewer.getProperties(
+          dbId,
+          (result) => resolve(result),
+          (error) => reject(error),
+        );
+      });
+    }
+
+    const data = [];
+
+    try {
+      const doorDbIds = await getDoorDbIds();
+      if (!doorDbIds || doorDbIds.length <= 0) {
+        throw new Error('No Rooms found in current model');
+      }
+
+      for (let i = 0; i < doorDbIds.length; i++) {
+        const dbId = doorDbIds[i];
+        const propData = await getPropertiesAsync(dbId);
+        let bbox = await this.getBoundingBox(dbId);
+
+        data.push({
+          id: propData.externalId,
+          dbId: dbId,
+          name: propData.name,
+          bbox: bbox
+        });
+      }
+
+    } catch (ex) {
+      console.warn(`[DoorListPanel]: ${ex}`);
+      throw new Error('Failed to extract door data');
+    }
+
+    return data;
+  }
+
   async getBoundingBox(dbId) {
     const model = this.viewer.model;
     const it = model.getInstanceTree();
@@ -136,9 +214,24 @@ class RoomsImagesExtension extends Autodesk.Viewing.Extension {
     return bounds;
   }
 
+  async getViewElements() {
+    const tool = this.viewer.getExtension('Autodesk.BoxSelection').boxSelectionTool;
+    const { left: startX, top: startY, right: endX, bottom: endY } = this.viewer.impl.getCanvasBoundingClientRect();
+    tool.startPoint.set(startX, startY);
+    tool.endPoint.set(endX, endY);
+    let selection = await tool.getSelection();
+    return selection;
+  }
+
   async generateThumbnail(viewObj) {
     await this.viewer.navigation.setView(viewObj.position, viewObj.target);
     await this.viewer.navigation.setCameraUpVector(new THREE.Vector3(0, 0, 1));
+    await this, viewer.setFocalLength(10);
+    let dbIdsinView = await this.getViewElements();
+    this.roomElements.push({
+      name: viewObj.name,
+      dbIdsinView: dbIdsinView[0].ids
+    })
     let vw = this.viewer.container.clientWidth;
     let vh = this.viewer.container.clientHeight;
     this.viewer.getScreenShot(vw, vh, blob => {
@@ -154,7 +247,20 @@ class RoomsImagesExtension extends Autodesk.Viewing.Extension {
       if (viewpointsCount > 0) {
         this.viewer.fitToView(viewObj.dbId);
       }
+      else {
+        this.downloadObjectAsJson(this.roomElements, 'RoomsElements');
+      }
     });
+  }
+
+  downloadObjectAsJson(exportObj, exportName) {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
   }
 
   async load() {
